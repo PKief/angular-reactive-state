1fd4d7ef2686270729bd0d6fef387bb709467d19
Philipp Kief <philipp.kief@gmx.de>
2022-11-25 20:22:44 +0100

    Implement unit tests

--- src/app/shared/store.spec.ts
+++ src/app/shared/store.spec.ts
@@ -1,10 +1,56 @@
+import { firstValueFrom } from 'rxjs';
 import { Store } from './store';
+
+class TestStore<T extends object> extends Store<T> {
+  dispatchAction(actionName: string, actionFn: (state: T) => T) {
+    this.changeState(actionName, actionFn);
+  }
+}
 
 describe('Store', () => {
   it('should set initial state for store', () => {
-    class TestStore extends Store<{ a: string }> {}
-    const store = new TestStore({ a: 'a' });
-    
+    const store = new TestStore<{ a: string }>({ a: 'a' });
     store.state$.subscribe((state) => expect(state.a).toBe('a'));
+  });
+
+  it('should select correct state out of store', () => {
+    const store = new TestStore<{ a: string }>({ a: 'a' });
+    store
+      .select((state) => state.a)
+      .subscribe((value) => expect(value).toBe('a'));
+  });
+
+  it('should subscribe to state changes', async () => {
+    const store = new TestStore<{ a: string }>({ a: 'a' });
+    const stateValue = await firstValueFrom(store.select((state) => state.a));
+    expect(stateValue).toBe('a');
+    store.dispatchAction('change to uppercase A', (state) => ({
+      ...state,
+      a: 'A',
+    }));
+    const updatedStateValue = await firstValueFrom(
+      store.select((state) => state.a)
+    );
+    expect(updatedStateValue).toBe('A');
+  });
+
+  it('should not trigger change for state properties which are not selected', () => {
+    const store = new TestStore<{ a: { a: string }; b: { b: string } }>({
+      a: { a: 'a' },
+      b: { b: 'b' },
+    });
+    let countStateChangesA = 0;
+    let countStateChangesB = 0;
+
+    store.select((state) => state.a).subscribe(() => countStateChangesA++);
+    store.select((state) => state.b).subscribe(() => countStateChangesB++);
+
+    store.dispatchAction('change to uppercase A', (state) => ({
+      ...state,
+      a: { a: 'A' },
+    }));
+
+    expect(countStateChangesA).toBe(2);
+    expect(countStateChangesB).toBe(1);
   });
 });
