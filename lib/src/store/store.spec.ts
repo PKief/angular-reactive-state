9d02fa098a9ed1ed4cf0faa9be8298905a95baf9
Philipp Kief <philipp.kief@gmx.de>
2022-11-27 18:19:22 +0100

    Update unit tests

--- lib/src/store/store.spec.ts
+++ lib/src/store/store.spec.ts
@@ -1,61 +1,75 @@
+import { TestBed } from '@angular/core/testing';
 import { firstValueFrom } from 'rxjs';
 import { Store } from './store';
 
-class TestStore<T extends object> extends Store<T> {}
+type TestStoreState = {
+  a: {
+    a: string;
+  };
+  b: {
+    b: string;
+  };
+};
+class TestStore extends Store<TestStoreState> {
+  constructor() {
+    super('TestStore', {
+      a: { a: 'a' },
+      b: { b: 'b' },
+    });
+  }
+}
 
 describe('Store', () => {
+  let store: TestStore;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [TestStore],
+    });
+    store = TestBed.inject(TestStore);
+  });
+
   it('should set initial state for store', () => {
-    const store = new TestStore<{ a: string }>('TestStore', { a: 'a' });
-    store.state$.subscribe((state) => expect(state.state.a).toBe('a'));
+    store.state$.subscribe(state => expect(state.state.a).toEqual({ a: 'a' }));
   });
 
   it('should select correct state out of store', () => {
-    const store = new TestStore<{ a: string }>('TestStore', { a: 'a' });
     store
-      .select((state) => state.a)
-      .subscribe((value) => expect(value).toBe('a'));
+      .select(state => state.a)
+      .subscribe(value => expect(value).toEqual({ a: 'a' }));
   });
 
   it('should subscribe to state changes', async () => {
-    const store = new TestStore<{ a: string }>('TestStore', { a: 'a' });
-    const stateValue = await firstValueFrom(store.select((state) => state.a));
-    expect(stateValue).toBe('a');
-    store.changeProperty('a', 'A');
+    const stateValue = await firstValueFrom(store.select(state => state.a));
+    expect(stateValue).toEqual({ a: 'a' });
+    store.changeProperty('a', { a: 'A' });
     const updatedStateValue = await firstValueFrom(
-      store.select((state) => state.a)
+      store.select(state => state.a)
     );
-    expect(updatedStateValue).toBe('A');
+    expect(updatedStateValue).toEqual({ a: 'A' });
   });
 
   it('should trigger state changes via custom actions', () => {
-    const store = new TestStore<{ a: string }>('TestStore', { a: 'a' });
     const stateValue = store.snapshot.a;
-    expect(stateValue).toBe('a');
-    store.dispatchAction('a', (state) => ({
+    expect(stateValue).toEqual({ a: 'a' });
+    store.dispatchAction('a', state => ({
       ...state,
-      a: 'A',
+      a: { a: 'A' },
     }));
     const updatedStateValue = store.snapshot.a;
-    expect(updatedStateValue).toBe('A');
+    expect(updatedStateValue).toEqual({ a: 'A' });
   });
 
   it('should not trigger change for state properties which are not selected', () => {
-    const store = new TestStore<{ a: { a: string }; b: { b: string } }>(
-      'TestStore',
-      {
-        a: { a: 'a' },
-        b: { b: 'b' },
-      }
-    );
     let countStateChangesA = 0;
     let countStateChangesB = 0;
 
-    store.select((state) => state.a).subscribe(() => countStateChangesA++);
-    store.select((state) => state.b).subscribe(() => countStateChangesB++);
+    store.select(state => state.a).subscribe(() => countStateChangesA++);
+    store.select(state => state.b).subscribe(() => countStateChangesB++);
 
     store.changeProperty('a', { a: 'A' });
 
-    expect(countStateChangesA).toBe(2);
-    expect(countStateChangesB).toBe(1);
+    expect(countStateChangesA).toEqual(2);
+    expect(countStateChangesB).toEqual(1);
   });
 });
