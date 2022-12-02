e3a5344e333e0bc488db2b0cd839be00d7939b55
Philipp Kief <philipp.kief@gmx.de>
2022-12-02 21:45:37 +0100

    Refactor code

--- lib/src/utils/distinctUntilObjectChanged.ts
+++ lib/src/utils/distinctUntilObjectChanged.ts
@@ -3,5 +3,5 @@ import { distinctUntilChanged, Observable } from 'rxjs';
 
 export const distinctUntilObjectChanged = () => {
   return <T>(source: Observable<T>): Observable<T> =>
-    source.pipe(distinctUntilChanged((prev, cur) => isEqual(prev, cur)));
+    source.pipe(distinctUntilChanged(isEqual));
 };
