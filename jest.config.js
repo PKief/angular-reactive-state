bd23954090f7f5178c35a925de73282904641b8c
Philipp Kief <philipp.kief@gmx.de>
2023-01-22 12:48:38 +0100

    Fix test setup

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,11 @@
 module.exports = {
   preset: 'jest-preset-angular',
   globalSetup: 'jest-preset-angular/global-setup',
+  setupFiles: ['<rootDir>/jest-setup.ts'],
+  roots: ['<rootDir>', 'lib/src'],
+  modulePaths: ['<rootDir>', 'lib/src'],
+  moduleDirectories: ['node_modules', 'lib/src'],
+  moduleNameMapper: {
+    'angular-reactive-state/(.*)': ['<rootDir>/lib/src/$1'],
+  },
 };
